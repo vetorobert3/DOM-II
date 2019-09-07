1c2091edf8eda8bd5ea9413f5dc86c561554b5c3
Veto Ramirez <vetorobert3@gmail.com>
2019-09-07 13:12:49 -0700

    trying to figure out dblclick.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 // Your code goes here
+
+//#1
+const newNavLink = document.querySelectorAll('.nav-link');
+
+newNavLink.forEach(function(navs) {
+  navs.addEventListener('click', function(event) {
+    event.target.style.color = '#1261ff'
+  });
+});
+
+//#2
+const headLogo = document.querySelector('.logo-heading');
+
+headLogo.addEventListener('mouseenter', function(event) {
+  event.target.style.transform = 'rotate(20deg)';
+});
+
+//#3
+headLogo.addEventListener('mouseout', (event) => {
+  event.target.style.transform = 'rotate(0deg)';
+});
+
+//#4
+const resizeImg = document.querySelector('.intro');
+
+resizeImg.addEventListener('mousedown', function(event) {
+  event.target.style.transform = "scale(1.3)"
+  event.target.style.transition = 'transform 0.5s'
+});
+
+//#5
+resizeImg.addEventListener('mouseup', (event) => {
+  event.target.style.transform = 'scale(1)'
+})
+
+//#6
+const keyColor = document.querySelector('.content-destination');
+
+keyColor.addEventListener('dblclick', (event) => {
+  event.target.style.backgroundColor = 'blue';
+});
